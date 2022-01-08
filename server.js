cc289a13ff100f076615a3b9c6330c67c6c60c01
mxcezl <54847071+mxcezl@users.noreply.github.com>
2022-01-08 19:29:02 +0100

    final commit

--- server.js
+++ server.js
@@ -9,23 +9,14 @@ const nocache = require('nocache')
 const PORT = 4848;
 const HOST = 'localhost';
 
-// openssl req -x509 -newkey rsa:4096 -keyout server_key.pem -out server_cert.pem -nodes -days 365 -subj "/CN=localhost/O=INSA"
-// Creation de server_key.pem et de server_cert.pem
 const opts = {
-    key: fs.readFileSync('./keys/server/server_key.pem'),
-    cert: fs.readFileSync('./keys/server/server_cert.pem'),
+    key: fs.readFileSync('./server-key.pem'),
+    cert: fs.readFileSync('./server-crt.pem'),
+    ca: fs.readFileSync('./ca-crt.pem'),
+	crl: fs.readFileSync('./ca-crl.pem'),
     requestCert: true,
-    rejectUnauthorized: false,
-    ca: fs.readFileSync('./keys/server/server_cert.pem'),
-	//crl: [
-	//	fs.readFileSync('./keys/server/server_cert.pem')
-	//]
+    rejectUnauthorized: true
 }
-// Creation de cle et certificat pour un utilisateur
-// openssl req -newkey rsa:4096 -keyout maxence_key.pem -out maxence_csr.pem -nodes -days 365 -subj "/CN=Maxence"
-
-// Signature du certificat avec la cle du server
-// openssl x509 -req -in maxence_csr.pem -CA server_cert.pem -CAkey server_key.pem -out maxence_cert.pem -set_serial 01 -days 365
 
 // App
 const app = express();
